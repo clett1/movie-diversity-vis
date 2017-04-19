d12be1ae24df2632ca2ed8bbb6645fd9e6d38999
clett1 <courtneylett2@gmail.com>
2017-04-18 23:06:31 -0400

    Updates for April 19th

--- movieVis.js
+++ movieVis.js
@@ -1,9 +1,25 @@
 //Global variable for all character data
 var characterData;
+var movieData;
 
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    width = 960,
+    height = 500;
+
+//xScale setup
+var xScale = d3.scaleLinear()
+                .range([margin.left, width - margin.right]);
+
+//yScale setup
+var yScale = d3.scaleBand()
+                .range([margin.top, height - margin.bottom]);
+
+d3.json('data/movieData.json', function(error, data) {
+    movieData = data;
+    //createMovieData();
+})
+
+console.log(movieData);
 
 //Read in data from CharacterData
 d3.csv("data/CharacterData.csv", function (data) {
@@ -12,139 +28,114 @@ d3.csv("data/CharacterData.csv", function (data) {
         // Convert numeric values to 'numbers'
         d.CHARACTER_WORDS = +d.CHARACTER_WORDS;
         d.TOTAL_MOVIE_WORDS = +d.TOTAL_MOVIE_WORDS;
+        d.wordsPercent = (+d.CHARACTER_WORDS / +d.TOTAL_MOVIE_WORDS) * 100;
     });
 
     // Store csv data in a global variable
     characterData = data;
-   
-    console.log(characterData);
     
+    createChart();
 });
 
-/* 
- * value accessor - returns the value to encode for a given data object.
- * scale - maps value to a visual display encoding, such as a pixel position.
- * map function - maps from data value to display value
- * axis - sets up axis
- */ 
+function createMovieData() {
+    
+    var roiArray = movieData.map(function(d) {
+        return d.value["roi"];
+    });
+    
+    //y scale (sorted by ROI)       
+    yScale.domain(roiArray);
+}
 
-// setup x 
-var xValue = function(d) { 
-    return d.Calories;
-    } // data -> value
 
-var xScale = d3.scale.linear().range([0, width]); // value -> display
+function createChart() {
 
-var xMap = function(d) { 
-    return xScale(xValue(d));
-    } // data -> display
+    var wordCountPercent = characterData.map(function(d) {
+        return (d.CHARACTER_WORDS / d.TOTAL_MOVIE_WORDS) * 100;
+    });
+    
+    var totalWords = characterData.map(function(d) {
+        return d.TOTAL_MOVIE_WORDS;
+    })
+    
+    //x scale axis (word spoken percentage)
+    xScale.domain([0, d3.max(wordCountPercent)]); 
+    
+    yScale.domain(totalWords);
+    /*
+    //X axis
+    var xAxis = d3.svg.axisBottom(xScale);
 
-var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
-
-// setup y
-var yValue = function(d) { return d["Protein (g)"];}, // data -> value
-    yScale = d3.scale.linear().range([height, 0]), // value -> display
-    yMap = function(d) { return yScale(yValue(d));}, // data -> display
-    yAxis = d3.svg.axis().scale(yScale).orient("left");
-
-// setup fill color
-var cValue = function(d) { return d.Manufacturer;},
-    color = d3.scale.category10();
-
-// add the graph canvas to the body of the webpage
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// add the tooltip area to the webpage
-var tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-
-// load data
-d3.csv("cereal.csv", function(error, data) {
-
-  // change string (from CSV) into number format
-  data.forEach(function(d) {
-    d.Calories = +d.Calories;
-    d["Protein (g)"] = +d["Protein (g)"];
-//    console.log(d);
-  });
-
-  // don't want dots overlapping axis, so add in buffer to data domain
-  xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
-  yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
-
-  // x-axis
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("x", width)
-      .attr("y", -6)
-      .style("text-anchor", "end")
-      .text("Calories");
+    //Y axis
+    var yAxis = d3.svg.axisLeft(yScale);
+    */
+    // add the graph canvas to the body of the webpage
+    var svg = d3.select("body").append("svg")
+        .attr("width", 960)
+        .attr("height", 500)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    /*
+     // x-axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("x", width)
+        .attr("y", -6)
+        .style("text-anchor", "end")
+        .text("% of Words Spoken");
 
   // y-axis
   svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Protein (g)");
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+      . text("Movie");
+    */
+    
+    //Create the circles for the graph
+    var circles = svg.selectAll("circle").data(characterData);
+    
+    circles = circles.enter()
+                .append('circle')
+                .merge(circles);
+    
+    circles.exit().remove();
+    
+    circles
+        .attr('cx', function(d) {            
+            return xScale(d.wordsPercent);
+            
+        })
+        .attr('cy', function(d) {
+            console.log(d.TOTAL_MOVIE_WORDS);
+            return yScale(d.TOTAL_MOVIE_WORDS);
+        })
+        .attr('r', function(d) {
+            return 3.5;
+        })
+        .attr('fill', function(d) {
+            return "black";
+        });
+    
+}
 
-  // draw dots
-  svg.selectAll(".dot")
-      .data(data)
-    .enter().append("circle")
-      .attr("class", "dot")
-      .attr("r", 3.5)
-      .attr("cx", xMap)
-      .attr("cy", yMap)
-      .style("fill", function(d) { return color(cValue(d));}) 
-      .on("mouseover", function(d) {
-          tooltip.transition()
-               .duration(200)
-               .style("opacity", .9);
-          tooltip.html(d["Cereal Name"] + "<br/> (" + xValue(d) 
-	        + ", " + yValue(d) + ")")
-               .style("left", (d3.event.pageX + 5) + "px")
-               .style("top", (d3.event.pageY - 28) + "px");
-      })
-      .on("mouseout", function(d) {
-          tooltip.transition()
-               .duration(500)
-               .style("opacity", 0);
-      });
-
+/*      
   // draw legend
   var legend = svg.selectAll(".legend")
       .data(color.domain())
-    .enter().append("g")
+      .enter().append("g")
       .attr("class", "legend")
       .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-  // draw legend colored rectangles
-  legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
+*/
 
-  // draw legend text
-  legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { return d;})
-});
 
