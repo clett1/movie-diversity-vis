e3012f9bb6906c25a862a1c6346dfd984802e36f
“Emily <essaltz@gmail.com>
2017-05-03 00:56:10 -0400

    introducing the head
    * index.html conflict resolution
    * head wrapper

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 $('#test-button').click(function () {
     $(this).toggleClass('blue');
 });
+
+var $doc = $(document),
+    $head = $('#beige-head'),
+    $nose = $('#nose-shape'),
+    y = $doc.scrollTop(),
+    color1 = 0,
+    color2 = 0;
+
+$doc.on('scroll', function(){
+  console.log("hi");
+  y = $doc.scrollTop();
+  color1 = y/2;
+  color2 = color1 + 180;
+  $head.css('fill', 'hsla(' + color1 + ', 85%, 75%, 1)');
+  $nose.css('fill', 'hsla(' + color1 + ', 55%, 75%, 1)');
+});
